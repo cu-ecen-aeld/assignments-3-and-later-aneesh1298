ae4d88f309a1277afd748d283f440ef54b8a7f7f
aneesh1298 <angu1298@colorado.edu>
2024-03-16 23:37:43 -0600

    changes

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -2,16 +2,15 @@
 
 case "$1" in
     start)
-        echo "Starting aesdsocket"
+        echo "Starting aesdsocket..."
         start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
         ;;
     stop)
-        echo "Stopping aesdsocket"
+        echo "Stopping aesdsocket..."
         start-stop-daemon -K -n aesdsocket
         ;;
     *)
         echo "Usage: $0 {start|stop}"
         exit 1
-	;;
+        ;;
 esac
-
